3b37339d100791d8534d6a165e4ba232db0d77bf
Philipp <Philipp.Hofmann2@stud.hs-coburg.de>
2026-03-09 16:49:17 +0100

    Initialize Spring Boot project for "PathmodelBackend". Includes Maven wrapper, base configuration, and boilerplate project structure.

---
+++ PathmodelBackend/src/main/java/org/philipp/fun/pathmodelbackend/PathmodelBackendApplication.java
@@ -0,0 +1,13 @@
+package org.philipp.fun.pathmodelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathmodelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathmodelBackendApplication.class, args);
+    }
+
+}
